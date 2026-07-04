6e740a6d415c99d4d16bb8ce7c16103ac03d7c66
Ananjana <ananjanak246@gmail.com>
2026-07-04 21:33:23 +0530

    level 2 programs

---
+++ grade_calculator.py
@@ -0,0 +1,9 @@
+marks=int(input("Enter your marks: "))
+if marks>=90:
+    print("A")
+elif 75<=marks<=89:
+    print("B")
+elif 50<=marks<=74:
+    print("C")
+else:
+    print("Fail")
---
+++ traffic_signal.py
@@ -0,0 +1,9 @@
+colour=str(input("Enter a colour (Red/Green/Yellow): ")).lower()
+if colour=="red":
+    print("Stop")
+elif colour=="yellow":
+    print("Ready")
+elif colour=="green":
+    print("Start")
+else:
+    print("Enter a valid colour")
