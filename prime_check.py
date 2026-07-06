c53e602f0a6e30c5de43f461f1ea557b3dda7f54
Ananjana <ananjanak246@gmail.com>
2026-07-06 23:47:48 +0530

    level-4 programs

---
+++ prime_check.py
@@ -0,0 +1,15 @@
+num=int(input("Enter a number: "))
+if num<=1:
+    print("Not prime")
+else:
+    is_prime=True
+    
+    for i in range(2,num):
+        if num%i==0:
+            is_prime=False
+            break
+    if is_prime:
+        print("It is prime")
+    else:
+        print("Not prime")
+
