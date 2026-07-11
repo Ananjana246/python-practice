fd99dd74c4f3b442128cbb856fadd8888fe130af
Ananjana <ananjanak246@gmail.com>
2026-07-11 20:42:48 +0530

    level-4 programs

---
+++ ex.py
@@ -0,0 +1,27 @@
+# num=0
+# for i in range(1,21):
+#     if i%2==0:
+#         print(i)
+
+# sum=0
+# for i in range(1,11):
+#     sum=sum+i
+# print(sum)
+
+# num=int(input("Enter a number: "))
+# for i in range(1,11):
+#     print(num,"*",i,"=",num*i)
+
+# for i in range(10,0,-1):
+#     print(i)
+
+# num=int(input("Enter a number: "))
+# fact=1
+# for i in range(1,num+1):
+#     fact=fact*i
+# print(fact)
+
+# for i in range(1,101):
+#     if i%3==0 and i%5==0:
+#         print(i)
+ 
---
+++ skip_divisible_4.py
@@ -0,0 +1,5 @@
+num=int(input("Enter the number to which it needs: "))
+for i in range(1,num+1):
+    if i%4==0:
+        continue
+    print(i)
