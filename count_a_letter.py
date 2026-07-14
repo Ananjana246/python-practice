d6dd151f991e89b722546c3465635ffe8c4320ca
Ananjana <ananjanak246@gmail.com>
2026-07-14 23:48:02 +0530

    level-4 programs

--- count_a_letter.py
+++ count_a_letter.py
@@ -1,7 +1,8 @@
-ext=str(input("Enter a string: "))
+text=str(input("Enter a string: "))
+letter=input("Enter a letteer: ")
 count=0
 for ch in text:
-    
-    
+    if ch==letter:
+        count=count+1
 print(count)
     
