73eda3818b7d291ca6299a87765adf522b7881bc
Ananjana <ananjanak246@gmail.com>
2026-08-05 21:00:56 +0530

    lambda programs

---
+++ map_lambda.py
@@ -0,0 +1,7 @@
+# List of strings
+words = ["apple", "banana", "orange", "grape"]
+
+# Convert to uppercase using lambda and map()
+uppercase_words = list(map(lambda x: x.upper(), words))
+
+print(uppercase_words)
